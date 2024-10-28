57d824e11f9e479315ad6fa0af4881b3169fd0f7
anup2000-cmd <68800453+anup2000-cmd@users.noreply.github.com>
2024-10-28 20:37:55 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -13,13 +13,7 @@ public class FirstTest {
     @Test
     public void OpenBrowser()  {
         System.out.println("Hiiiii-----");
-       //WebDriver driver;
-      //  System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-     //   ChromeOptions options = new ChromeOptions();
-      //  options.addArguments("disable-gpu");
-      // ChromeOptions chromeOptions = new ChromeOptions();
-      WebDriverManager.firefoxdriver().setup();
-    WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new FirefoxDriver();
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
         //Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
