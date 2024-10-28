280fa1ca5212b7b21e08a4707b8f0a14c78a99fb
anup2000-cmd <68800453+anup2000-cmd@users.noreply.github.com>
2024-10-28 20:59:07 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -21,8 +21,8 @@ public class FirstTest {
         WebDriver driver;
        // System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
       //  ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        options.addArguments("disable-gpu");
+      //  options.addArguments("headless");
+       // options.addArguments("disable-gpu");
         driver = new FirefoxDriver();
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
